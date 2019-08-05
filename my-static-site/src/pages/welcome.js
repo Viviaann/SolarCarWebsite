e0d176403f96f25954110f8e2de9b940649468ec
Simon <simonfong6@gmail.com>
2019-08-04 23:23:17 -0700

    Added comments for styles

--- my-static-site/src/pages/welcome.js
+++ my-static-site/src/pages/welcome.js
@@ -3,18 +3,21 @@ import React from 'react'
 import backgroundImage from '../images/welcome-banner.jpg'
 
 const welcomeStyle = {
+  // Makes the background image cover the full page.
   "backgroundImage": "url(" + backgroundImage + ")",
   "height": "100%",
   "backgroundPosition": "center",
   "backgroundRepeat": "no-repeat",
   "backgroundSize": "cover",
 
+  // Makes title centered.
   "display": "flex",
   "justifyContent": "center",
   "alignItems": "center"
 }
 
 const titleStyle = {
+  // Adds a contrast for the white text.
   "background": "rgba(0, 0, 0)",
   "background": "rgba(0, 0, 0, 0.40)",
   "paddingLeft": "20px",
