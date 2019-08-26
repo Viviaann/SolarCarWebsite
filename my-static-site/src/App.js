80dd4b592d0706c70e572139ed40e1c26106c695
Simon <simonfong6@gmail.com>
2019-08-26 00:03:22 -0700

    Added favicon

--- my-static-site/src/App.js
+++ my-static-site/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Root, Routes, addPrefetchExcludes } from 'react-static'
-//
-import { Link, Router } from 'components/Router'
+import { Root, Routes, addPrefetchExcludes, Head } from 'react-static'
+import { Router } from 'components/Router'
 import Dynamic from 'containers/Dynamic'
 
 import './css/bootstrap.min.css'
 import './app.css'
+
+import favicon from 'images/tabcar.png'
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
 addPrefetchExcludes(['dynamic'])
@@ -13,12 +14,15 @@ addPrefetchExcludes(['dynamic'])
 function App() {
   return (
     <Root>
-        <React.Suspense fallback={<em>Loading...</em>}>
-          <Router>
-            <Dynamic path="dynamic" />
-            <Routes path="*" />
-          </Router>
-        </React.Suspense>
+      <Head>
+        <link rel="shortcut icon" href={ favicon } />
+      </Head>
+      <React.Suspense fallback={<em>Loading...</em>}>
+        <Router>
+          <Dynamic path="dynamic" />
+          <Routes path="*" />
+        </Router>
+      </React.Suspense>
     </Root>
   )
 }
