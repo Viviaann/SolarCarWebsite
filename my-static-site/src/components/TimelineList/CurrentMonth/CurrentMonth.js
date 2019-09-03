cea3d57a3ee2816f8129191057072024b901388f
Simon <simonfong6@gmail.com>
2019-09-03 14:23:56 -0700

    Added Basic Current Month

--- my-static-site/src/components/TimelineList/CurrentMonth/CurrentMonth.js
+++ my-static-site/src/components/TimelineList/CurrentMonth/CurrentMonth.js
@@ -5,6 +5,12 @@ export default function CurrentMonth(props) {
   return (
     <div>
       <h1>Current Month</h1>
+      <h3>
+        <span>
+          { props.month }&nbsp;{ props.year }
+        </span>
+      </h3>
+
     </div>
   )
 };
