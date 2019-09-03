15ec8c0da4e0da3303c99fc8033d79ca30894cf9
Simon <simonfong6@gmail.com>
2019-09-03 14:25:02 -0700

    Created event card

--- my-static-site/src/components/TimelineList/Event/Event.js
+++ my-static-site/src/components/TimelineList/Event/Event.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default function Event(props) {
 
   return (
     <div>
-      <h1>Event</h1>
+      <Card style={{ width: '100%' }}>
+        <Row noGutters={ true }>
+          <Col md="4">
+            <Card.Img variant="left" src={ props.img } />
+          </Col>
+          <Col md="8">
+            <Card.Body>
+              <Card.Title>{ props.title }</Card.Title>
+              <Card.Text>
+                <small class="text-muted">{ props.date }</small>
+              </Card.Text>
+              <Card.Text>
+              { props.description }
+              </Card.Text>
+              
+            </Card.Body>
+            </Col>
+          </Row>
+      </Card>
     </div>
   )
 };
