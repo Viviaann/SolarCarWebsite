a345bcb976c57e19db7086fc57d043f32876d760
Simon <simonfong6@gmail.com>
2019-09-03 00:43:14 -0700

    Basic year selector

--- my-static-site/src/components/YearSelector/YearSelector.js
+++ my-static-site/src/components/YearSelector/YearSelector.js
@@ -5,6 +5,14 @@ export default function YearSelector(props) {
   return (
     <div>
       <h1>Year Selector</h1>
+      <label>Year</label>&nbsp;
+      <select>
+        <option value="2015">2015</option>
+        <option value="2016">2016</option>
+        <option value="2017">2017</option>
+        <option value="2018">2018</option>
+        <option value="2019">2019</option>
+      </select>
     </div>
   )
 };
