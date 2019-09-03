562a47cab217ac7df6e05213b4be13cac7e7b1d7
Simon <simonfong6@gmail.com>
2019-09-03 14:26:07 -0700

    Added one event

--- my-static-site/src/components/TimelineList/TimelineList.js
+++ my-static-site/src/components/TimelineList/TimelineList.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import Event from './Event'
+import CurrentMonth from './CurrentMonth'
+
+import GroupPhotoSoldering from '../../images/group-photo-soldering.jpg'
 
 export default function TimelineList(props) {
 
   return (
     <div>
       <h1>TimelineList</h1>
+      <CurrentMonth
+        month="May"
+        year="2019"
+      />
+      <Event
+        img={ GroupPhotoSoldering }
+        title="First workday of the summer!"
+        date="July 27, 2019"
+        description="A beautiful day to be training members to solder tabs on the solar cells."
+      />
     </div>
   )
 };
