737c5d899963b296dc54bd5720013d51ab3271d8
Simon <simonfong6@gmail.com>
2019-09-04 02:29:04 -0700

    Added Coastal Enterprises event

--- my-static-site/src/components/TimelineList/TimelineList.js
+++ my-static-site/src/components/TimelineList/TimelineList.js
@@ -3,6 +3,7 @@ import Event from './Event'
 import CurrentMonth from './CurrentMonth'
 
 import GroupPhotoSoldering from '../../images/group-photo-soldering.jpg'
+import CoastalEnterprisesImage from '../../images/coastal-enterprises.jpg'
 
 export default function TimelineList(props) {
 
@@ -18,6 +19,12 @@ export default function TimelineList(props) {
         title="First workday of the summer!"
         date="July 27, 2019"
         description="A beautiful day to be training members to solder tabs on the solar cells."
+      />
+      <Event
+        img={ CoastalEnterprisesImage }
+        title="Coastal Enterprises"
+        date="May 31, 2019 "
+        description="Visited Chuck Miller from Coastal Enterprises to pick up Milling Foam + Adhesives to test"
       />
     </div>
   )
