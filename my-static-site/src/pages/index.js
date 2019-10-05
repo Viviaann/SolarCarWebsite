acf00ef809dd673ebc8517a166b994cf9c36e77d
Simon <simonfong6@gmail.com>
2019-10-05 00:43:49 -0700

    Imports welcome component

--- my-static-site/src/pages/index.js
+++ my-static-site/src/pages/index.js
@@ -1,11 +1,1 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-
-export default () => (
-  <div>
-    <Navbar />
-    <div style={{ textAlign: 'center' }}>
-      <h1>Welcome to React-Static</h1>
-    </div>
-  </div>
-)
+export {default} from './welcome';
