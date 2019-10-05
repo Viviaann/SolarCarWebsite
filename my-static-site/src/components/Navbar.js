74c9529f866714d0a462c93b99cfc80a99749d26
Simon <simonfong6@gmail.com>
2019-10-05 13:32:45 -0700

    Made Navbar collapsible

--- my-static-site/src/components/Navbar.js
+++ my-static-site/src/components/Navbar.js
@@ -1,13 +1,42 @@
 import React from 'react'
+import { Head } from 'react-static'
 import { Link } from 'components/Router'
+
+import '../js/jquery-3.3.1.slim.min.js'
 
 export default  function Navbar() {
   return (
-    <nav className="navbar navbar-light" >
-      <Link to="/welcome">Welcome</Link>
-      <Link to="/team">Team</Link>
-      <Link to="/timeline">Timeline</Link>
-      <Link to="/donations">Donations</Link>
-    </nav>
+    <div>
+      <Head>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      </Head>
+      <nav className="navbar navbar-expand-lg navbar-dark" >
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/welcome">Welcome</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/team">Team</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/timeline">Timeline</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/donations">Donations</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+
+      </nav>
+
+    </div>
   )
 };
